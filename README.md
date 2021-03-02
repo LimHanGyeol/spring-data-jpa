67e9bbbfefed716d420cdc19ca0217f33cc4f309
임한결 <dlagksruf19@naver.com>
2021-03-02 10:44:15 +0900

    Initial Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-data-jpa
+
 스프링 데이터 JPA 학습
---
+++ src/main/java/com/tommy/datajpa/DatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatajpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatajpaApplication.class, args);
+    }
+
+}
